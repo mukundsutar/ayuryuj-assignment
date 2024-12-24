b7c88e5b0a126ce252bdbd62f674957721c67705
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2024-12-25 01:55:55 +0530

    Wrapper setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,26 @@
+import "../global.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router/stack";
 import { PaperProvider } from "react-native-paper";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
 	return (
-		<PaperProvider>
-			<Stack>
-				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-			</Stack>
-		</PaperProvider>
+		<QueryClientProvider client={queryClient}>
+			<PaperProvider>
+				<Stack>
+					<Stack.Screen
+						name="(tabs)"
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="edit"
+						options={{ headerShown: false }}
+					/>
+				</Stack>
+			</PaperProvider>
+		</QueryClientProvider>
 	);
 }
