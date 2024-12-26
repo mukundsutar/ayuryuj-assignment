9d9ddf5f5ca2eb7110c4b925e51b0dfb485c9804
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2024-12-27 01:41:34 +0530

    moved data to respective files for relevance

---
+++ lib/data.ts
@@ -0,0 +1,33 @@
+// home
+
+// doctor-listing
+export const specialization: string[] = [
+	"Anesthesiology",
+	"Cardiology",
+	"Dermatology",
+	"Emergency Medicine",
+	"Family Medicine",
+	"Forensic Pathology",
+	"General Surgery",
+	"Genetics and Genomics",
+	"Hospital Medicine",
+	"Internal Medicine",
+	"Neurology",
+	"Neurological Surgery",
+	"Ophthalmic Surgery",
+	"Orthopaedic Surgery",
+	"Otolaryngology",
+	"Pathology",
+	"Pediatrics",
+	"Plastic Surgery",
+	"Preventive Medicine",
+	"Psychiatry",
+	"Radiology",
+	"Rheumatology",
+	"Sleep Medicine",
+	"Thoracic Surgery",
+	"Urology",
+	"Vascular Surgery",
+];
+
+// profile
