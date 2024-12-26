7993015cf3c2cfc48bbacbbef76798253f68fbff
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2024-12-27 01:45:26 +0530

    Removed react-native-paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider } from "@shopify/restyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router/stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { PaperProvider } from "react-native-paper";
 
 import { DrawerProvider } from "../lib/providers/drawer-provider";
 import { FontsProvider } from "../lib/providers/font-provider";
@@ -19,39 +18,36 @@ export default function RootLayout() {
 			<FontsProvider>
 				<QueryClientProvider client={queryClient}>
 					<DrawerProvider>
-						<PaperProvider>
-							<BottomSheetModalProvider>
-								<ThemeProvider theme={THEME}>
-									<Stack
-										screenOptions={{
-											animation: "fade_from_bottom",
-											animationTypeForReplace: "push",
+						<BottomSheetModalProvider>
+							<ThemeProvider theme={THEME}>
+								<Stack
+									screenOptions={{
+										animation: "fade_from_bottom",
+										animationTypeForReplace: "push",
+									}}
+								>
+									<Stack.Screen
+										name="(tabs)"
+										options={{ headerShown: false }}
+									/>
+									<Stack.Screen
+										name="edit"
+										options={{
+											title: "Edit your Profile",
+											headerBackVisible: true,
+											presentation: "modal",
 										}}
-									>
-										<Stack.Screen
-											name="(tabs)"
-											options={{ headerShown: false }}
-										/>
-										<Stack.Screen
-											name="edit"
-											options={{
-												title: "Edit your Profile",
-												headerBackVisible: true,
-												presentation: "modal",
-											}}
-										/>
-										<Stack.Screen
-											name="doctor"
-											options={{
-												headerShown: false,
-												presentation:
-													"transparentModal",
-											}}
-										/>
-									</Stack>
-								</ThemeProvider>
-							</BottomSheetModalProvider>
-						</PaperProvider>
+									/>
+									<Stack.Screen
+										name="doctor"
+										options={{
+											headerShown: false,
+											presentation: "transparentModal",
+										}}
+									/>
+								</Stack>
+							</ThemeProvider>
+						</BottomSheetModalProvider>
 					</DrawerProvider>
 				</QueryClientProvider>
 			</FontsProvider>
