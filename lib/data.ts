055583f3da2cf2768a2277a41a17f1616313faf3
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2025-01-02 02:49:15 +0530

    Added new data

--- lib/data.ts
+++ lib/data.ts
@@ -30,4 +30,64 @@ export const specialization: string[] = [
 	"Vascular Surgery",
 ];
 
+export const women = [
+	"Period related issue",
+	"Pregnancy related issue",
+	"White Discharge",
+	"Excessive bleeding",
+	"Pregnancy Planning",
+];
+
+export const skin = [
+	"Pimple/Acne",
+	"Hairfall",
+	"Itching",
+	"Spots on Skin",
+	"Rashes",
+	"Dry Scalp/Dandruff",
+	"Dark circles",
+	"Reddish Skin",
+	"Pigmentation and Melasma",
+	"Acne and Scar",
+	"Hair Care",
+	"Skin Care",
+	"Skin-Peels and Polish",
+	"Anti aging/Rejuvenation",
+	"Dull Skin",
+	"Under eye puffiness",
+	"Fine lines",
+	"Wrinkles",
+	"Uneven skin tone",
+	"Sun damage",
+	"Uneven texture",
+	"Dry Skin",
+	"Oily Skin",
+];
+
+export const common = [
+	"Fever",
+	"Blocked nose",
+	"Cough",
+	"Runny nose",
+	"Throat pain",
+	"Headache",
+	"Loose motion",
+	"Constipation",
+	"Gas",
+	"Vomiting/Nausea",
+	"High Cholesterol",
+	"Diabetes",
+	"High BP",
+	"Low BP",
+	"Migraine",
+	"Back Pain",
+];
+
+export const diet = [
+	"Diet for Gaining Weight",
+	"Diet for Weight Loss",
+	"Diet for Diabetes",
+	"Diet for Hypertension",
+];
+
 // profile
