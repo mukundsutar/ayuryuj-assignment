a3a36fb70752598fa79e3e637a1ef8fa751b8bcd
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2025-01-02 02:49:07 +0530

    Removed unused font import

--- lib/providers/font-provider.tsx
+++ lib/providers/font-provider.tsx
@@ -1,16 +1,8 @@
 import {
-	Roboto_100Thin,
-	Roboto_100Thin_Italic,
 	Roboto_300Light,
-	Roboto_300Light_Italic,
 	Roboto_400Regular,
-	Roboto_400Regular_Italic,
 	Roboto_500Medium,
-	Roboto_500Medium_Italic,
 	Roboto_700Bold,
-	Roboto_700Bold_Italic,
-	Roboto_900Black,
-	Roboto_900Black_Italic,
 } from "@expo-google-fonts/roboto";
 import { useFonts } from "expo-font";
 import { SplashScreen } from "expo-router";
@@ -21,18 +13,10 @@ SplashScreen.preventAutoHideAsync();
 
 export function FontsProvider({ children }: { children: React.ReactNode }) {
 	const [loaded] = useFonts({
-		Roboto_100Thin,
-		Roboto_100Thin_Italic,
 		Roboto_300Light,
-		Roboto_300Light_Italic,
 		Roboto_400Regular,
-		Roboto_400Regular_Italic,
 		Roboto_500Medium,
-		Roboto_500Medium_Italic,
 		Roboto_700Bold,
-		Roboto_700Bold_Italic,
-		Roboto_900Black,
-		Roboto_900Black_Italic,
 	});
 
 	useEffect(() => {
